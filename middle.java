eef8bc545943430cc29ad0618b34965826d6a221
Elango Radhakrishnan <rkrishnaelango@gmail.com>
2024-10-14 11:30:56 +0530

    inital commit

--- middle.java
+++ middle.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class middle{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);  
+    System.out.println("Enter the Number :");
         int size = sc.nextInt();
         int [] arr  = new int [size];
 for(int i =0;i<=size-1;i =i+1){
-    // System.out.println("Enter the Number :");
     arr[i] = sc.nextInt();
 }
 System.out.println("Middele number is "+ arr[size/2]);
